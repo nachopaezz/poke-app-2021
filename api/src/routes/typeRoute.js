ff6ca44292ea5ce3759615a4c6f84b84aa3a0acd
Nacho <ignaciopaez96@gmail.com>
2021-09-07 12:12:16 -0300

    test5

--- api/src/routes/typeRoute.js
+++ api/src/routes/typeRoute.js
@@ -1,13 +1,22 @@
+const { default: axios } = require('axios');
 const { Router } = require('express');
-const {getTypeApi} = require("../controllersFunctions/getType")
 const {Type} = require('../db')
 
 const router = Router()
 
-module.exports = router
+
+router.get('/', async (req, res) => {
+    try{const infoApitype = await axios.get('https://pokeapi.co/api/v2/type');  // Entra a la API, me trae la info
+    const infoType = infoApitype.data.results.map(elem => elem.name); // Mapea y devuelve muchos arreglos
+    const createType = infoType.forEach(elem => {  // Una vez mapeado, hace un findOrCreate y me guarda esas ocupaciones en el modelo.
+      Type.findOrCreate({    // Si no lo encuentra, lo crea. Si lo encuentra lo deja pasar.
+        where: {name : elem}  // Donde el nombre sea este elemento q estoy mapeando.
+      })
+      });
+    const typeFinal = await Type.findAll();
+    res.send(typeFinal);}
+    catch(error){console.log(error)}
+  })
 
 
-router.get('/', async (req,res,next)=>{
-    const result = await getTypeApi()
-    res.json(result)
-})
+module.exports = router;
